3634849b04e59f948030d92b51806652f47cb018
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2020-04-02 16:41:15 +0530

    adding comments

--- src/selectionsort/SelectionSort.java
+++ src/selectionsort/SelectionSort.java
@@ -13,15 +13,15 @@ public class SelectionSort {
         int temp=0;
         int n = array.length;
         for(i=0;i<n;i++){
-            int k = i;
-            for(int j = i;j<n-1;j++){//j<n-1 kyuki ham j+1 use kar rahe hain to index out of bound exception aa jayega
+            int k = i;            // k = starting INDEX diya hai to jab bhi koi chotra number milega to is k pe us chote number vaale ka indexsave ho jayega to phir ham swap karlenge
+            for(int j = i;j<n-1;j++){         //j<n-1 kyuki ham j+1 use kar rahe hain to index out of bound exception aa jayega
 
                 if(array[k]>array[j+1]){
-                    k= j+1;
+                    k= j+1;               // kamain chote number ki index store kar di
 
                 }
             }
-            temp=array[i];
+            temp=array[i];        //swaping
             array[i]=array[k];
             array[k]=temp;
         }
