17e9c6b867e8624685970a4d5147ce41f53d64cb
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2020-04-02 16:28:38 +0530

    constructed sort method for bubble sorting

--- src/bubblesort/BubbleSort.java
+++ src/bubblesort/BubbleSort.java
@@ -14,7 +14,21 @@ public class BubbleSort {
      *
      * @param array the array to be sorted
      */
+
     public void sort(int[] array) {
+        int temp=0;
+        int k = array.length;
+        for(int i=0;i<k;i++){
+            for(int j=0;j<k-1;j++){
+                if (array[j] > array[j+1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
 
     }
+
+
 }
