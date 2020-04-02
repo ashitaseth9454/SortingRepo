6af37958aeff8b456fac49cd0c7171adf60f7be7
Ashita vijay seth <ashita.seth_18@gla.ac.in>
2020-04-02 16:38:29 +0530

    constructed sort method for selection sorting

--- src/selectionsort/SelectionSort.java
+++ src/selectionsort/SelectionSort.java
@@ -9,6 +9,23 @@ package selectionsort;
 
 public class SelectionSort {
     public void sort(int[] array) {
+        int i=0;
+        int temp=0;
+        int n = array.length;
+        for(i=0;i<n;i++){
+            int k = i;
+            for(int j = i;j<n-1;j++){//j<n-1 kyuki ham j+1 use kar rahe hain to index out of bound exception aa jayega
+
+                if(array[k]>array[j+1]){
+                    k= j+1;
+
+                }
+            }
+            temp=array[i];
+            array[i]=array[k];
+            array[k]=temp;
+        }
 
     }
-}
+
+    }
